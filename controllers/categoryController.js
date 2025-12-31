31e3d2ec712d039f00a53eaad7eee09fdcd643a2
Taner <tanerdokmets@gmail.com>
2025-12-31 15:10:19 +0300

    updated

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,4 @@
-import Category from "../models/Category.js";
+import Category from "../models/category.js";
 import { sendSuccess, sendError } from "../utils/responseHandler.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
